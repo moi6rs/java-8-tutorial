027ccd4d2259ba8636d1d6674e9ad2a554c6a136
mramoss <moi6ramoss@gmail.com>
2019-06-12 10:15:33 -0400

    sum of n numbers the squared

--- Java8-tutorial/src/java8/tutorial/Java8Tutorial.java
+++ Java8-tutorial/src/java8/tutorial/Java8Tutorial.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package java8.tutorial;
 
-/**
- *
- * @author ramseg
- */
 import java.util.Arrays;
 import java.util.List;
 public class Java8Tutorial {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         // TODO code application logic here
@@ -23,8 +11,11 @@ public class Java8Tutorial {
         int[] array = {30, 10, 20, 40};
         newReduceToSum.sumArray(array);
         
-        List<Integer> list = Arrays.asList(30, 10, 20, 40);
+        List<Integer> list = newReduceToSum.createLisNumberSquared(2);
         newReduceToSum.sumList(list);
+        
+        
+        
     }
     
 }
--- Java8-tutorial/src/java8/tutorial/ReduceToSum.java
+++ Java8-tutorial/src/java8/tutorial/ReduceToSum.java
@@ -1,27 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package java8.tutorial;
 
-/**
- *
- * @author ramseg
- */
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 public class ReduceToSum {
+
     private Integer numberLentgh;
-    
-    public void sumArray(int[] array){
-        int sum = Arrays.stream(array).reduce(0, (x,y) -> x + y);
-        System.out.println("Sum of Array: "+ sum);
+
+    public void sumArray(int[] array) {
+        int sum = Arrays.stream(array).reduce(0, (x, y) -> x + y);
+        System.out.println("Sum of Array: " + sum);
     }
-    
-    public void sumList(List<Integer> list){
-        int sum = list.stream().reduce(0, (x,y) -> x + y);
-        System.out.println("Sum List: "+ sum);
+
+    public void sumList(List<Integer> list) {
+        int sum = list.stream().reduce(0, (x, y) -> x + y);
+        System.out.println("Sum List: " + sum);
     }
-            
+
+    public List<Integer> createLisNumberSquared(int numberLength) {
+        List<Integer> newListNumber = new ArrayList<Integer>();
+        for (int i = 1; i <= numberLength; i++) {
+            double num = Math.pow(i, 2);
+            newListNumber.add((int) num);
+        }
+        return newListNumber;
+    }
 }
